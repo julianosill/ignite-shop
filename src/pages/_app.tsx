82f9a100a776bd269da4f54e38509d0aa8932603
Juliano Sill <sill.juliano@gmail.com>
2023-12-31 20:30:44 -0300

    style: set global styles using stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
-
 import { Roboto } from 'next/font/google'
 
 const roboto = Roboto({
     weight: ['400', '700'],
     subsets: ['latin'],
 })
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
