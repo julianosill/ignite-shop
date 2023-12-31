83eb1e6c9858d3aecc838097ad028b4f068175da
Juliano Sill <sill.juliano@gmail.com>
2023-12-31 20:42:44 -0300

    feat(Header): create header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import { Roboto } from 'next/font/google'
+
+import logo from '../assets/ignite-logo.svg'
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
 
 const roboto = Roboto({
     weight: ['400', '700'],
@@ -17,7 +21,12 @@ export default function App({ Component, pageProps }: AppProps) {
                     font-family: ${roboto.style.fontFamily};
                 }
             `}</style>
-            <Component {...pageProps} />
+            <Container>
+                <Header>
+                    <Image src={logo} alt="" />
+                </Header>
+                <Component {...pageProps} />
+            </Container>
         </>
     )
 }
