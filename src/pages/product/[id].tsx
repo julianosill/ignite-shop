8d8058f666e592a68437a3bf493205a9424a65fa
Juliano Sill <sill.juliano@gmail.com>
2024-01-02 21:24:51 -0300

    feat(product): create page details

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,79 @@
-import { useRouter } from 'next/router'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import Stripe from 'stripe'
 
-export default function Product() {
-    const { query } = useRouter()
-    return <h1>Product {query.id}</h1>
+import { stripe } from '@/lib/stripe'
+import {
+    ImageContainer,
+    ProductContainer,
+    ProductDetails,
+} from '@/styles/pages/product'
+import Image from 'next/image'
+
+interface ProductProps {
+    product: {
+        id: string
+        imageUrl: string
+        name: string
+        price: string
+        description: string
+    }
+}
+
+export default function Product({ product }: ProductProps) {
+    return (
+        <ProductContainer>
+            <ImageContainer>
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>{product.name}</h1>
+                <span>{product.price}</span>
+                <p>{product.description}</p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            {
+                params: { id: 'prod_PIWsqjIcgBWmin' },
+            },
+        ],
+        fallback: false,
+    }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+    params,
+}) => {
+    const productId = params!.id
+
+    const product = await stripe.products.retrieve(productId, {
+        expand: ['default_price'],
+    })
+
+    const price = product.default_price as Stripe.Price
+
+    return {
+        props: {
+            product: {
+                id: product.id,
+                imageUrl: product.images[0],
+                name: product.name,
+                price:
+                    price.unit_amount &&
+                    new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL',
+                    }).format(price.unit_amount / 100),
+                description: product.description,
+            },
+        },
+        revalidate: 60 * 60 * 1,
+    }
 }
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -6,6 +6,7 @@ export const HomeContainer = styled('main', {
     width: '100%',
     maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
     minHeight: 656,
+    padding: '0 1.5rem',
     marginLeft: 'auto',
 })
 
