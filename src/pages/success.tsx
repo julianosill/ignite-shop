71fca4ac157a329a645cc323b77a2733f004e9c6
Juliano Sill <sill.juliano@gmail.com>
2024-01-02 23:03:58 -0300

    feat(success): redirect when session_id is missing

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -32,6 +32,15 @@ export default function Sucess({ customerName, product }: SuccessProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    if (!query.session_id) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false,
+            },
+        }
+    }
+
     const sessionId = String(query.session_id)
     const session = await stripe.checkout.sessions.retrieve(sessionId, {
         expand: ['line_items', 'line_items.data.price.product'],
